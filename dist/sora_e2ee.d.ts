0f8a86bcf4ae45dda555f886a4779cbd410c1827
Yuki Ito <yuito@shiguredo.jp>
2020-05-15 10:48:23 +0900

    成果物を更新する

--- dist/sora_e2ee.d.ts
+++ dist/sora_e2ee.d.ts
@@ -1,8 +1,9 @@
 declare class SoraE2EE {
     worker: Worker | null;
-    constructor();
+    masterKey: ArrayBuffer;
+    onWorkerDisconnect: Function | null;
+    constructor(masterSecret: string);
     startWorker(): void;
-    setupKey(): void;
     setupSenderTransform(sender: RTCRtpSender): void;
     setupReceiverTransform(receiver: RTCRtpReceiver): void;
 }
